d15f607e5e80bece01fce277d6f18cd987e5ee3b
krishna <jeewanjotsingh09@gmail.com>
2017-05-20 23:42:26 -0400

    javafile

--- app/src/main/java/com/example/a1691589/td_canada/MainActivity.java
+++ app/src/main/java/com/example/a1691589/td_canada/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.a1691589.td_canada;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv = (TextView) findViewById(R.id.textView9);
+        ImageView im = (ImageView) findViewById(R.id.imageView5);
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
+                startActivity(intent);
+            }
+
+
+        });
+
+        im.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
+                startActivity(intent);
+            }
+
+        });
     }
 }
